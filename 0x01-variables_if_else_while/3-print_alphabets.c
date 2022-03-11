f5bbd0ca832bfdb0412289221052d5bab276ac19
Victoria-20 <victoriaocon111@gmail.com>
2022-03-11 17:21:45 +0300

    Print upper and lower alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
+        {
+                putchar(alphabet);
+        }
+
+	putchar('\n');
+	return (0);
+
+}
