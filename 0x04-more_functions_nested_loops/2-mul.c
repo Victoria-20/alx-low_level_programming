e92ea6789810f75fe785de6b64641d96c39d57e0
Victoria-20 <victoriaocon111@gmail.com>
2022-03-18 11:10:31 +0300

    Multiply 2 intergers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* mul - Multiplies two intergers
+* @a: interger 1
+* @b: interger 2
+* Return: 0 if successful
+*/
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
