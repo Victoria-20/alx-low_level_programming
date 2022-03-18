f49aeef179d336bfe1ed228a2d7b03f076ba67a2
Victoria-20 <victoriaocon111@gmail.com>
2022-03-18 12:26:39 +0300

    Print most numbers except 2&4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_most_numbers - Prints most  numbers except 2 and 4
+*
+* Return: 0 if successful
+*/
+void print_most_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i != '2' && i != '4')
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,3 +2,5 @@ int _putchar(char);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int, int);
+void print_numbers(void);
+void print_most_numbers(void);
