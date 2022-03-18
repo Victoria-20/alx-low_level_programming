5ef74b38fe4df12462514aaf773692d79a0f11b7
Victoria-20 <victoriaocon111@gmail.com>
2022-03-18 21:24:12 +0300

    Fizz Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - Prints numbers 10 times
+*
+* Return: 0 if successful
+*/
+
+int main(void)
+{
+	int n;
+
+	n = 1;
+	printf("%d", n);
+
+	for (n = 2; n <= 100; n++)
+	{
+		if ((n % 3 == 0) && (n % 5 == 0))
+		{
+			printf(" FizzBuzz");
+		}
+		else if (n % 3 == 0)
+		{
+			printf(" Fizz");
+		}
+		else if (n % 5 == 0)
+		{
+			printf(" Buzz");
+		}
+		else
+		{
+			printf(" %d", n);
+		}
+	}
+	printf("\n");
+	return (0);
+}
