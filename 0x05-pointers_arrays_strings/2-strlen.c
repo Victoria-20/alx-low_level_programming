0cd4b63f6cd4a4db93e749e110fc78aa46c96ff6
Victoria-20 <victoriaocon111@gmail.com>
2022-03-21 20:47:35 +0300

    Return length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - updates the value of variable n using pointers
+ * @s: int variable to be swapped
+ * Return: Always 0
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(s);
+	return (len);
+}
