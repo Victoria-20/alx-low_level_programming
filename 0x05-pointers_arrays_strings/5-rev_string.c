e1fe71e7aeb8aa14cf05d30a7837adff5966eb6e
Victoria-20 <victoriaocon111@gmail.com>
2022-03-22 22:10:49 +0300

    Reverse a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,6 +9,37 @@
 
 void rev_string(char *s)
 {
-	(strrev(*s));
-	_putchar('\n');
+	char *start_c, *end_c, c;
+	int i, count;
+	int length = 0;
+
+	for (i = 0; s[i]; i++)
+	{
+		length++;
+	}
+
+	count = length;
+
+	start_c = s;
+	end_c = s;
+
+
+	for (i = 0; i < count - 1; i++)
+	{
+	end_c++;
+	}
+
+
+	for (i = 0; i < count / 2; i++)
+	{
+
+
+	c = *end_c;
+	*end_c = *start_c;
+	*start_c = c;
+
+
+	start_c++;
+	end_c--;
+	}
 }
