55f812c25168ced5463c6d6896afce92241a735b
Victoria-20 <victoriaocon111@gmail.com>
2022-03-24 21:36:12 +0300

    Lowercase to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* string_toupper - reverse the contents of an array of intergers
+* @str: string
+* Return: string
+*/
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
+}
