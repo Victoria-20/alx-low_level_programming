257d76ccc250f1e415cb1fa843da01956b64d698
Victoria-20 <victoriaocon111@gmail.com>
2022-03-24 17:31:55 +0300

    Compare 2 strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* _strcmp - compare 2 strings
+* @s1: first string
+* @s2: second string
+* Return: 0 if successfull
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	int result;
+
+	result = strcmp(s1, s2);
+	return (result);
+}
