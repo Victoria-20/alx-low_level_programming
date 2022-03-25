e3c6f4c7ebf311943ec5acd37b0a71e59499fbdf
Victoria-20 <victoriaocon111@gmail.com>
2022-03-25 20:49:09 +0300

    Reverse a string

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,17 +1,20 @@
 #include "main.h"
-#include <string.h>
 
 /**
-* _strcmp - compare 2 strings
-* @s1: first string
-* @s2: second string
-* Return: 0 if successfull
-*/
-
-int _strcmp(char *s1, char *s2)
+ * reverse_array - reverses the content of an array of integers.
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ *
+ * Return: nothing.
+ */
+void reverse_array(int *a, int n)
 {
-	int result;
+	int i, j, t;
 
-	result = strcmp(s1, s2);
-	return (result);
+	for (i = 0, j = (n - 1); i < j; i++, j--)
+	{
+		t = a[i];
+		a[i] = a[j];
+		a[j] = t;
+	}
 }
