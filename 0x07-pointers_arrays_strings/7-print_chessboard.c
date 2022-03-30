46e337474192cc41c22cf075910e53bbce647263
Victoria-20 <victoriaocon111@gmail.com>
2022-03-30 10:39:51 +0300

    Print the chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,13 +1,25 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
-* _memset - fills memory with a constant byte
-* s:
-* b:
-* n:
+* print_chessboard- fills memory with a constant byte
+* @a: 2D array of array
+* Return: void
 */
 
-char *_strstr(char *haystack, char *needle)
+void print_chessboard(char (*a)[8])
 {
+	int i, j;
 
+	i = 0;
+	while (i < 8)
+	{
+		j = 0;
+		while (j < 8)
+		{
+			_putchar(a[i][j]);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
 }
--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,13 +1,30 @@
 #include <stdio.h>
 
 /**
-* _memset - fills memory with a constant byte
-* s:
-* b:
-* n:
+* print_diagsums - fills memory with a constant byte
+* @a: interger pointer
+* @size: size of array
+* Return: void
 */
 
-char *_strstr(char *haystack, char *needle)
+void print_diagsums(int *a, int size)
 {
+	/*Declaring variables*/
+	int i;
+	int sum1, sum2;
 
+	sum1 = 0;
+	sum2 = 0;
+
+	i = 0;
+	while (i < size) /*number repetitions*/
+	{
+		/*sums for diagsums*/
+		sum1 = sum1 + *(a + i * size + i);
+		sum2 = sum2 + *(a + i * size + size - i - 1);
+
+		i++; /*add +1*/
+	}
+
+	printf("%i, %i\n", sum1, sum2);
 }
