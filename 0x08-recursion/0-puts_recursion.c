5fbb1e338840e79db5eef2d9f337c368127572a5
Victoria-20 <victoriaocon111@gmail.com>
2022-03-31 15:30:22 +0300

    Print a string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * _puts_recursion - prints a string followed by a new line
@@ -8,5 +9,15 @@
 
 void _puts_recursion(char *s)
 {
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	else
+	{
+		_putchar(*s);
+	_puts_recursion(s + 1);
+	}
 
 }
