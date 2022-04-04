70bd2f6df7bc2304d6b3c5375167ac8adbe8a4b9
Victoria-20 <victoriaocon111@gmail.com>
2022-04-04 11:40:41 +0300

    Print all arguments

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,12 +1,20 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * whatsmyname - program that prints its name
+ * main - program that prints its name
  * @argc: counts number of arguments on the command line
  * @argv: array of pointer to the arguments passed(strings)
+ * Return: 0 if successful
  */
 
-int whatsmyname(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
 	return (0);
 }
