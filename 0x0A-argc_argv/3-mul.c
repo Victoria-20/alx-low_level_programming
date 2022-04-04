5deacd3d67aa72e9a5b283199eae8a58df544524
Victoria-20 <victoriaocon111@gmail.com>
2022-04-04 12:21:08 +0300

    Multiply 2 numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,12 +1,24 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * whatsmyname - program that prints its name
+ * main - program that prints its name
  * @argc: counts number of arguments on the command line
  * @argv: array of pointer to the arguments passed(strings)
+ * Return: 0 if successful
  */
 
-int whatsmyname(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
+	int mul = 0;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	mul = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", mul);
 	return (0);
 }
