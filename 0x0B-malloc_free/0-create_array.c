a1e53cec2eea18c34ea0f6747023e293cac89c36
Victoria-20 <victoriaocon111@gmail.com>
2022-04-04 17:25:13 +0300

    Create array of chars

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * create_array - creates an array of chars
+ * @size: size of the array
+ * @c: character c
+ * Return: pointer to the array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	int *ptr;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	ptr = c;
+	return (ptr);
+}
---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * create_array - creates an array of chars
+ * @size: size of the array
+ * @c: character c
+ */
+
+char *create_array(unsigned int size, char c)
+{
+
+}
