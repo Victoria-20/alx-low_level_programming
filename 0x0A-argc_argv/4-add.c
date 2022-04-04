f0f0d8b78290252e6573703e18ea524f167c46c0
Victoria-20 <victoriaocon111@gmail.com>
2022-04-04 10:32:04 +0300

    Add all files

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * whatsmyname - program that prints its name
+ * @argc: counts number of arguments on the command line
+ * @argv: array of pointer to the arguments passed(strings)
+ */
+
+int whatsmyname(int argc, char *argv[])
+{
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,7 @@
+## C argc, argv & Unused Variables
+
+### Tasks
+#### 0. Program that prints its name
+Solution [0-whatsmyname.c](0-whatsmyname.c)
+
+#### 1. 
