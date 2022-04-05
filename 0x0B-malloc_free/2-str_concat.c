55965778bceee568d86f338432266249ca52e5af
Victoria-20 <victoriaocon111@gmail.com>
2022-04-05 13:32:02 +0300

    Concatenate 2 strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,46 +1,43 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
- * str_concat - creates an array of chars
- * @s1: pointer to string 1
- * @s2: pointer to string 2
-* Return: newly allocated memory
+ * str_concat - concatenates two strings.
+ * @s1: first string.
+ * @s2: second string.
+ *
+ * Return: pointer of an array of chars
  */
-
 char *str_concat(char *s1, char *s2)
 {
-	char *dst;
-	unsigned int i, j, size;
+	char *strout;
+	unsigned int i, j, k, limit;
 
-	/*If the array is empty*/
 	if (s1 == NULL)
 		s1 = "";
-
 	if (s2 == NULL)
 		s2 = "";
 
-	/*count size total*/
-	size = (_strlen(s1) + _strlen(s2) + 1);
+	for (i = 0; s1[i] != '\0'; i++)
+		;
 
-	/*malloc*/
-	dst = (char *) malloc(size * sizeof(char));
+	for (j = 0; s2[j] != '\0'; j++)
+		;
 
-	if (dst == 0)
+	strout = malloc(sizeof(char) * (i + j + 1));
+
+	if (strout == NULL)
 	{
+		free(strout);
 		return (NULL);
 	}
 
-	/*Concatenate arrays*/
-	for (i = 0; *(s1 + i) != '\0'; i++)
-		*(dst + i) = *(s1 + i);
+	for (k = 0; k < i; k++)
+		strout[k] = s1[k];
 
-	for (j = 0; *(s2 + j) != '\0'; j++)
-	{
-		*(dst + i) = *(s2 + j);
-		i++;
-	}
+	limit = j;
+	for (j = 0; j <= limit; k++, j++)
+		strout[k] = s2[j];
 
-	return (dst);
+	return (strout);
 }
