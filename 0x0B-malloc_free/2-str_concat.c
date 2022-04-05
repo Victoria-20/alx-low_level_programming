61c82007931b742bbe5fa4a16b4dccf54c230eb1
Victoria-20 <victoriaocon111@gmail.com>
2022-04-05 13:26:13 +0300

    Concatenate 2 strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,39 +11,36 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	char *ptr;
+	char *dst;
 	unsigned int i, j, size;
 
-	/*check if arrays are empty */
+	/*If the array is empty*/
 	if (s1 == NULL)
-	{
-		s1 = ""
-	}
+		s1 = "";
+
 	if (s2 == NULL)
-	{
-		s2 = ""
-	}
+		s2 = "";
 
-	/* calculate total size of arrays */
-	size = (strlen(s1) + strlen(s2) + 1)
+	/*count size total*/
+	size = (_strlen(s1) + _strlen(s2) + 1);
 
-	/* allocate new memory */
-	ptr = (char *)malloc(size * sizeof(char));
+	/*malloc*/
+	dst = (char *) malloc(size * sizeof(char));
 
-	if (ptr == 0)
+	if (dst == 0)
 	{
 		return (NULL);
 	}
-	/* Concatenate the strings*/
+
+	/*Concatenate arrays*/
 	for (i = 0; *(s1 + i) != '\0'; i++)
-		*(ptr + i) = *(s1 + i);
+		*(dst + i) = *(s1 + i);
 
 	for (j = 0; *(s2 + j) != '\0'; j++)
 	{
-		*(ptr + i) = *(s2 + j);
+		*(dst + i) = *(s2 + j);
 		i++;
 	}
 
-	return (ptr);
-}
+	return (dst);
 }
