be131f0dcd5e43fe7eda113f182a3c9f99d6eb68
Victoria-20 <victoriaocon111@gmail.com>
2022-04-05 13:19:44 +0300

    Concatenate two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * str_concat - creates an array of chars
@@ -9,5 +11,39 @@
 
 char *str_concat(char *s1, char *s2)
 {
+	char *ptr;
+	unsigned int i, j, size;
 
+	/*check if arrays are empty */
+	if (s1 == NULL)
+	{
+		s1 = ""
+	}
+	if (s2 == NULL)
+	{
+		s2 = ""
+	}
+
+	/* calculate total size of arrays */
+	size = (strlen(s1) + strlen(s2) + 1)
+
+	/* allocate new memory */
+	ptr = (char *)malloc(size * sizeof(char));
+
+	if (ptr == 0)
+	{
+		return (NULL);
+	}
+	/* Concatenate the strings*/
+	for (i = 0; *(s1 + i) != '\0'; i++)
+		*(ptr + i) = *(s1 + i);
+
+	for (j = 0; *(s2 + j) != '\0'; j++)
+	{
+		*(ptr + i) = *(s2 + j);
+		i++;
+	}
+
+	return (ptr);
+}
 }
