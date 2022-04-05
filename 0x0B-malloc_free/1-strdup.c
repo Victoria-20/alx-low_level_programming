169bf0b1437626a69fdb7ad21248c5e4dd941984
Victoria-20 <victoriaocon111@gmail.com>
2022-04-05 13:41:50 +0300

    New allocated space in memory

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,39 +1,51 @@
 #include "main.h"
-#include <string.h>
 #include <stdlib.h>
 
 /**
- * _strdup - return a pointer to a newly allocated space in memory
- * @str: string
- * Return: pointer to duplicate string
+ * _strlen - This function returns the length of a string
+ * @s: Variable that contains a character array to be counted
+ * Return: Length of the string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		continue;
+
+	return (i);
+}
+
+
+/**
+ * _strdup - This function returns a pointer to a newly allocated space
+ * in memory, which contains a copy of the string given as a parameter
+ * @str: The string to copy
+ * Return: A pointer to the newly allocated space, NULL if it fails
  */
 
 char *_strdup(char *str)
 {
-	char *ptr;
-	int len;
+	char *copy;
+	unsigned int i;
 
-	/* Determine the length of the string*/
-	len = strlen(str);
-
-	/* Check for string */
+	/* if str is NULL, return NULL */
 	if (str == NULL)
-	{
 		return (NULL);
-	}
 
-	/* Allocate new memory */
-	ptr = malloc(len + 1);
+	/* allocate enough memory for copy */
+	copy = malloc(_strlen(str) + 1);
 
-	/* if insufficient memory, return NULL*/
-	if (ptr == NULL)
-	{
+	/* if malloc fails, return NULL */
+	if (copy == NULL)
 		return (NULL);
-	}
-	else
-	{
-		/*copy string str to ptr*/
-		strcpy(ptr, str);
-	}
-	return (ptr);
+
+	/* copy string inside str to copy */
+	for (i = 0; str[i] != '\0'; i++)
+		copy[i] = str[i];
+
+	/* append null terminator to copy */
+	copy[i] = '\0';
+
+	return (copy);
 }
