f083feb88dac9b8fee217e37aad2786e3be89bc4
Victoria-20 <victoriaocon111@gmail.com>
2022-04-14 11:43:04 +0300

    Add all files

---
+++ 0x0F-function_pointers/3-calc.h
@@ -0,0 +1,4 @@
+#ifndef _3-CALC_H
+#define _3-CALC_H
+
+endif /* _3-CALC_H */
---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: name to be printed
+ * @f: function pointer
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+
+}
