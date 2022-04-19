53bcfe780d7a15a71a699699578984c46fc2ee2c
Victoria-20 <victoriaocon111@gmail.com>
2022-04-19 09:34:14 +0300

    Include guard in header file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef _FUNCTION_POINTERS_H
-#define _FUNCTION_POINTER_H
+#ifndef _FUNCTION_POINTERS_
+#define _FUNCTION_POINTERS_
 
 #include <stdlib.h>
 
@@ -8,4 +8,4 @@ void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
-#endif /* _FUNCTION_POINTER_H */
+#endif /* _FUNCTION_POINTERs_ */
