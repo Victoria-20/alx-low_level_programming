9409d724d5e0d18f3f1699a9bd5f41ceb7eba8ef
Victoria-20 <victoriaocon111@gmail.com>
2022-04-27 17:16:26 +0300

    Add all project files

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_listint - print all elements of a listint_t list
+ * @h: pointer to the first node(head)
+ *
+ * Return: number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+
+}
