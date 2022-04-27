f21a746ba27cfc5310fc77ce989709cacb9bd411
Victoria-20 <victoriaocon111@gmail.com>
2022-04-27 17:54:21 +0300

    Return number of elements

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,13 +1,13 @@
 #include "lists.h"
 
 /**
- * print_listint - print all elements of a listint_t list
+ * listint_len - print all elements of a listint_t list
  * @h: pointer to the first node(head)
  *
  * Return: number of nodes
  */
 
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 	size_t n_nodes = 0;
 
