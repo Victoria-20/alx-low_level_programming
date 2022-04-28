6b69433a995b4d09c67ef32538bcf2fc33700a84
Victoria-20 <victoriaocon111@gmail.com>
2022-04-28 08:07:16 +0300

    Add new node at beginning

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,13 +1,28 @@
-#include "main.h"
+#include "lists.h"
 
 /**
- * print_listint - print all elements of a listint_t list
- * @h: pointer to the first node(head)
- *
- * Return: number of nodes
+ * add_nodeint - print all elements of a listint_t list
+ * @head: pointer to the first node(head)
+ * @n: number of elements
+ * Return: address of the new element
  */
 
-size_t print_listint(const listint_t *h)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
+	/* Create new node */
+	listint_t *new_node = malloc(sizeof(listint_t));
+
+	/* check if memory has been allocated */
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (*head);
+
 
 }
