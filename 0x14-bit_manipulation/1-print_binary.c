3d0a1fb20d0887f961a81c6bd4bea42620a8e074
Victoria-20 <victoriaocon111@gmail.com>
2022-04-30 11:26:12 +0300

    Print binary rep

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,11 +1,21 @@
 #include "main.h"
 
 /**
-* binary_to_uint - converts a binary number to an unsigned int
-* @b: pointer to string of characters
-* Return: converted number
+* print_binary - prints the binary representation of a number
+* @n: number
+* Return: void
 */
 
-unsigned int binary_to_uint(const char *b)
+void print_binary(unsigned long int n)
 {
+	if (n >> 0)
+	{
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+	else
+	{
+		_putchar('0');
+	}
 }
