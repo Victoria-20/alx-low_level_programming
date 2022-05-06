2cc3da99f7424d8f7683e62f3d6394982d6ef21a
Victoria-20 <victoriaocon111@gmail.com>
2022-05-06 18:17:47 +0300

    Read file & print

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: file to read
+ * @letters: number of letters to read
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+}
