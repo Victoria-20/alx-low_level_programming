93019d009573552f4fd688e86524582a672fa8a4
Victoria-20 <victoriaocon111@gmail.com>
2022-06-06 13:55:08 +0300

    Advanced

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,50 @@
+/**
+* _strlen_recursion - function
+*
+* @s: the chaine
+* Return: Always 0.
+*/
+
+int _strlen_recursion(char *s)
+{
+
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+* palidrone - function
+*
+* @s: chaine of the char
+* @taille: the number
+* @i: the number
+* Return: Always 0.
+*/
+int palidrone(char *s, int taille, int i)
+{
+	if (s[i] != '\0')
+	{
+		if (s[i] != s[taille - i])
+			return (0);
+		else
+			return (palidrone(s, taille, i + 1));
+	}
+	else
+		return (1);
+}
+
+/**
+* is_palindrome - function
+*
+* @s: the chaine of char
+* Return: Always 0.
+*/
+
+int is_palindrome(char *s)
+{
+	if (s[0] == '\0')
+		return (1);
+	return (palidrone(s, _strlen_recursion(s) - 1, 0));
+}
