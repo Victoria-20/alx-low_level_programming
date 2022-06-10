9043c14605dbe19000a969346c2a3e6248f1b514
Victoria-20 <victoriaocon111@gmail.com>
2022-06-10 15:32:58 +0300

    Fix header file

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -32,3 +32,4 @@ int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
+#endif
